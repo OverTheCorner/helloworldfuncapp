6bfe0899cdd583c5519cd6cbd022dd683edbec4e
OverTheCorner <gabrielalmario99@gmail.com>
2023-10-30 18:27:54 +0800

    only accept request with name

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -10,14 +10,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         try:
             req_body = req.get_json()
         except ValueError:
-            pass
+            logging.error("Error: No name passed")
+            return func.HttpResponse("Error: no name passed", status_code=400)
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.", status_code=200)
